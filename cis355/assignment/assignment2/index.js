571b0d04445ef3e07dbeb0fc4c5754f41dd0f125
sliang880 <sliang@svsu.edu>
2026-03-23 23:28:28 -0400

    Add files via upload

--- cis355/assignment/assignment2/index.js
+++ cis355/assignment/assignment2/index.js
@@ -5,21 +5,22 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
 
     if(req.url === '/'){
-        fs.readFile(path.join(__dirname,`..`,'assignment1',`1.html`)),
-        (err, content) => {
+        fs.readFile(path.join(__dirname, '..', 'assignment1', '1.html'),(err, content) => {
                                     
                                     if (err) throw err;
                                     res.writeHead(200, { 'Content-Type': 'text/html' });
                                     res.end(content);
-                        }
+                        })
+        
         
     }else if (req.url === '/api'){
-        fs.readFile(path.join(__dirname,`..`,'assignment1',`data.json`)),
-        (err,content)=>{
+
+        fs.readFile(path.join(__dirname, '..', 'assignment1', 'data.json'),(err,content)=>{
             if(err) throw err;
             res.writeHead(200,{'Content-Type':'application/json'});
             res.end(content);
-        }
+        })
+        
     }
 })
 
